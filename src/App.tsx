3e3568b99963f990f949e74888be3cb177ed7c63
Gunt Chanmas <gunt4826@outlook.com>
2022-12-28 21:43:33 +0700

    Add bootstrap and open-meteo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const weatherConfig = require("./other/open-meteo.json");
+	const [weatherData, setWeatherData] = useState<any>(null);
+
+	async function getWeather() {
+		const weather = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${weatherConfig.lat}&longitude=${weatherConfig.long}&hourly=${weatherConfig.hourly}&daily=${weatherConfig.daily}&timezone=${weatherConfig.timezone}`);
+		setWeatherData(weather);
+	}
+
+	getWeather();
+
+	return (
+		<div className="container">
+			{/* App header */}
+			<div className="row">
+				<h2 className="app-title">Weather Forecast</h2>
+			</div>
+
+			{/* Hourly forecast */}
+			<div className="row">
+				
+			</div>
+
+			{/* Daily forecast */}
+			<div className="row">
+
+			</div>
+		</div>
+	);
 }
 
 export default App;
